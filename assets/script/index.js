138a39782ec2aa58cf12d696e0c18185d0ca3ace
Bakhram Saparbaev <bakhromsaparbaev@gmail.com>
2024-07-13 15:14:35 +0500

    relise

--- assets/script/index.js
+++ assets/script/index.js
@@ -42,8 +42,79 @@ window.addEventListener('DOMContentLoaded', () => {
 
    const compositionText = document.querySelectorAll('.composition-item-text');
 
-   //compositionText.forEach(item => {
-   //   if(item)
-   //})
+   const handleMore = (element, text, moreBtn, closeBtn) => {
+      element.textContent = `${text}`;
+      moreBtn.remove();
+      element.append(closeBtn)
+   }
+
+   const hadnleClose = (element, text, moreBtn, closeBtn) => {
+      element.textContent = `${text}... `;
+      closeBtn.remove();
+      element.appendChild(moreBtn)
+   }
+
+
+   compositionText.forEach(item => {
+      if (item.textContent.length > 312) {
+         const text = item.textContent.slice(0, 312);
+         const fullText = item.textContent;
+
+         const moreBtn = document.createElement('span');
+         const closeBtn = document.createElement('span');
+
+         moreBtn.classList.add('more');
+         moreBtn.textContent = 'ещё';
+         moreBtn.addEventListener('click', () => {
+            handleMore(item, fullText, moreBtn, closeBtn);
+         })
+
+         closeBtn.classList.add('more');
+         closeBtn.textContent = 'свернуть';
+         closeBtn.addEventListener('click', () => {
+            hadnleClose(item, text, moreBtn, closeBtn);
+         })
+
+
+         item.innerHTML = `${text}... `;
+         item.appendChild(moreBtn)
+      }
+   });
+
+
+   const nameInput = document.getElementById('order-user-name');
+   const phoneInput = document.getElementById('order-user-number');
+
+   // Функция для создания подсказки
+   function createTooltip(input, text) {
+      const tooltip = document.createElement('div');
+      tooltip.classList.add('tooltip'); // Добавьте класс для стилизации
+      tooltip.textContent = text;
+      input.parentNode.appendChild(tooltip);
+
+      // Позиционирование подсказки
+      tooltip.style.position = 'absolute';
+      tooltip.style.top = `${input.offsetTop - 30}px`; // 5px отступ сверху
+      tooltip.style.right = `${input.offsetWidth + 5}px`; // 5px отступ справа
+      tooltip.style.backgroundColor = '#fff'; // Фон подсказки
+      tooltip.style.padding = '5px';
+      tooltip.style.borderRadius = '5px';
+      tooltip.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.1)'; // Тень
+      tooltip.style.zIndex = '1'; // Чтобы подсказка была поверх инпута
+
+      // Скрыть подсказку при потере фокуса
+      input.addEventListener('blur', () => {
+         tooltip.remove();
+      });
+   }
+
+   // Добавляем подсказки при фокусе
+   nameInput.addEventListener('focus', () => {
+      createTooltip(nameInput, 'Иван Иванов');
+   });
+
+   phoneInput.addEventListener('focus', () => {
+      createTooltip(phoneInput, '+7 (999) 123-45-67');
+   });
 
 });
